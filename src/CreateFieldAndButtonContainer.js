3d87191dd72c93a799c7e50a0b891ed80607e733
Thomas Cooper <thomascooper@Thomass-MBP.fios-router.home>
2022-10-16 12:35:00 -0400

    Add error notification for blank field.

--- src/CreateFieldAndButtonContainer.js
+++ src/CreateFieldAndButtonContainer.js
@@ -6,10 +6,20 @@ export default function CreateFieldAndButtonContainer({
   setToDoList,
   setNewItemName,
 }) {
+  const [fieldError, setFieldErrorStyling] = useState("no-field-error-styling");
+
   function HandleAddItem() {
-    const nextListItem = [...toDoList, { id: toDoList.length + 1, itemName: newItemName }];
-    setToDoList(nextListItem);
-    setNewItemName("");
+    if (newItemName.trim() === "") {
+      setFieldErrorStyling("field-error-styling");
+    } else {
+      setFieldErrorStyling("no-field-error-styling");
+      const nextListItem = [
+        ...toDoList,
+        { id: toDoList.length + 1, itemName: newItemName },
+      ];
+      setToDoList(nextListItem);
+      setNewItemName("");
+    }
   }
 
   return (
@@ -18,6 +28,7 @@ export default function CreateFieldAndButtonContainer({
         type="text"
         value={newItemName}
         onChange={(e) => setNewItemName(e.target.value)}
+        className={fieldError}
       />
       <input type="submit" value="Add Item" onClick={HandleAddItem} />
     </div>
