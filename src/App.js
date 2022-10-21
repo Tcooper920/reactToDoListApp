c450acfc32639c0af0d7321c7cbab37f89105899
Thomas Cooper <thomascooper@Thomass-MBP.fios-router.home>
2022-10-20 22:29:04 -0400

    Cleanup toDoList state.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import CreateToDoListApp from "./CreateToDoListApp";
 
-const toDoItemList = [];
-
 export default function App() {
   return (
     <div id="my-app">
-      <CreateToDoListApp toDoItemList={toDoItemList} />
+      <CreateToDoListApp />
     </div>
   );
 }
